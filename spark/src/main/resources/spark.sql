1d91673298e0cfdecf742328dbc80158afafb4a3
Iris-2333 <62918695+Iris-2333@users.noreply.github.com>
2022-11-28 17:45:01 +0800

    Delete spark directory

--- spark/src/main/resources/spark.sql
+++
@@ -1,9 +0,0 @@
-use contribution;
-DROP TABLE IF EXISTS `spark`;
-CREATE TABLE `spark` (
-                         `project` varchar(255) NOT NULL,
-                         `author` varchar(255) NOT NULL,
-                         `score` double NOT NULL,
-                         `rate` double NOT NULL,
-                         PRIMARY KEY (`project`,`author`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
