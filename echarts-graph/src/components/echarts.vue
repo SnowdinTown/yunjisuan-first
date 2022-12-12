61edbb5ca745bf2efdd8f4b4404ac7fb5f5bfcac
Iris <429328398@qq.com>
2022-12-12 19:12:07 +0800

    modify EChart Title

--- echarts-graph/src/components/echarts.vue
+++ echarts-graph/src/components/echarts.vue
@@ -92,8 +92,16 @@ export default {
       var myChart = echarts.init(document.getElementById("echarts"));
       const option = {
         title: {
-          text: this.list[this.index], // 系列名称，用于tooltip的显示，legend 的图例筛选，在 setOption 更新数据和配置项时用于指定对应的系列。
-          fontSize: 22
+          text: 'Ranking',
+          subtext: this.list[this.index], // 系列名称，用于tooltip的显示，legend 的图例筛选，在 setOption 更新数据和配置项时用于指定对应的系列。
+          borderWidth: 1,
+          itemGap: 15,
+          textStyle: {
+            fontSize: 30
+          },
+          subtextStyle: {
+            fontSize: 20
+          }
         },
         color: [
           '#FFC0CB'
